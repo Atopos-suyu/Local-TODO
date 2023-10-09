85d14dabf98e105eabc55cc9c512a2664c232628
Atopos <143861703+Atopos-suyu@users.noreply.github.com>
2023-10-09 08:25:11 +0800

    Update Application.java
    添加注释

--- j200-6-2_1-master/src/main/java/org/vaadin/marcus/spring/Application.java
+++ j200-6-2_1-master/src/main/java/org/vaadin/marcus/spring/Application.java
@@ -7,11 +7,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 /**
  * The entry point of the Spring Boot application.
  */
-@SpringBootApplication
+@SpringBootApplication  //标记这是一个Spring Boot应用程序的主类
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);//Spring Boot应用程序的启动方法
     }
-
-}
+}  //表示一个基本的Spring Boot应用程序入口类，用于启动和运行Spring Boot应用程序
